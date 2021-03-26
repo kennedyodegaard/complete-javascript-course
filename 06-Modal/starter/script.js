349873f6d36b6a0a5f26a6577e628b03c24e6a8b
Kennedy Odegaard <kennedyaodegaard@gmail.com>
2021-03-26 22:21:51 +0100

    guess my number, moda, pig game

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+const toggleModal = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+
+    toggleModal();
+  });
+
+btnCloseModal.addEventListener('click', toggleModal);
+overlay.addEventListener('click', toggleModal);
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode === 27) {
+    toggleModal();
+  }
+});
+
+// overlay.addEventListener('click', function)
